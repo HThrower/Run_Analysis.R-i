1a82f689ba87de2a00c3de5d6ca5f0c6df00845d
HThrower <31675832+HThrower@users.noreply.github.com>
2017-11-02 20:28:08 -0400

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,61 +1,45 @@
 
+library(reshape2)
+filename <- "getdata_dataset.zip"
 
-activity <- read_csv("~/R/UCI HAR Dataset.csv/activity.csv")
-View(activity)
-#steps per day
-total.steps <- aggregate(steps ~ date, data = activity , sum, na.rm = TRUE)
-hist(total.steps$steps,col="red",
-      main="Histogram of Total Steps taken per day",xlab="Total Steps taken per day",cex.axis=1,cex.lab = 1)
-##What is mean/median total number of steps taken per day? 10766 & 10765
-mean(total.steps$steps)
-median(total.steps$steps)
-print(mean)
-##What is the average daily activity pattern? 835 maximum number of steps and 
-##206.2average number of steps taken
-steps_interval <- aggregate(steps ~ interval, data = activity, mean, na.rm = TRUE)
-plot(steps ~ interval, data = steps_interval, type = "l", xlab = "Time Intervals (5-minute)", ylab = "Mean number of steps taken (all Days)", 
-     main = "Average number of steps Taken at 5 minute Intervals",  col = "yellow")
-maxStepInterval<-steps_interval[which.max(steps_interval$steps),"interval"]
-max.step<-max(steps_interval$average_steps)
-
-##Imputing missing values
-## 2304 total number of missing values
-missing_rows <- sum(!complete.cases(k))
-complete.k <- activity
-
-MedianStepsPerInterval <- function(interval){
-  steps_interval[steps_interval$interval==interval,"steps"]
+## Download and unzip the dataset:
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+  download.file(fileURL, filename, method="curl")
 }
-  flag = 0
-  for (i in 1:nrow(complete.k)) {
-    if (is.na(complete.k[i,"steps"])) {
-      complete.k[i,"steps"] <- MeanStepsPerInterval(complete.k[i,"interval"])
-      flag = flag + 1
-    }
-  }
-  total_steps <- aggregate(steps ~ date, data = complete.k, sum)
-  hist(total_steps$steps, col = "pink",xlab = "Total Number of Steps", 
-       ylab = "Frequency", main = "Histogram of Total Number of Steps taken each Day")
-##     the mean is the same as before however, 
-##the median is different from the begining based on the added missing values
 
-mean(total_steps$steps)
-median(total_steps$steps)
-print(median)
+#load the dataset(Training/test data)
+train<- read_csv("~/R/UCI HAR Dataset.csv/UCI HAR Dataset/train/X_train.txt")
+trainActivity<- read_csv("~/R/UCI HAR Dataset.csv/UCI HAR Dataset/train/y_train.txt")
+trainSubject<-subject_train <- read_csv("~/R/UCI HAR Dataset.csv/UCI HAR Dataset/train/subject_train.txt")
+train<-cbind(trainActivity,trainSubject,train)
+  
+test<- X_test <- read_csv("~/R/UCI HAR Dataset.csv/UCI HAR Dataset/test/X_test.txt")
+testActivities <- read_csv("~/R/UCI HAR Dataset.csv/UCI HAR Dataset/test/y_test.txt")
+subject_test <- read_csv("~/R/UCI HAR Dataset.csv/UCI HAR Dataset/test/subject_test.txt")
+test<-cbind(test, testActivities, subject_test)
 
-complete.k$day <- ifelse(as.POSIXlt(as.Date(complete.k$date))$wday%%6==
-                                  0, "weekend", "weekday")
-complete.k$day <- factor(complete.k$day, levels = c("weekday", "weekend"))
+#Mean of dataset
+featuresWanted <- grep(".*mean.*|.*std.*", features[,2])
+featuresWanted.names <- features[featuresWanted,2]
+featuresWanted.names = gsub('-mean', 'Mean', featuresWanted.names)
 
-steps.interval= aggregate(steps ~ interval + day, complete.k, mean)
-library(lattice)
-xyplot(steps ~ interval | factor(day), data = steps.interval, aspect = 1/2, 
-       type = "l")
+#merge of data set
+MergTotalData<-cbind(train,test)
+colnames(allData) <- c("subject", "activity", featuresWanted.names)
 
 
+#The factor value of  (Activity/features)
+activityName<- read_csv("~/R/Getting and Cleaning Data Course Project/UCI HAR Dataset/activity_labels.txt")
+activityName<- as.character(activityLabels)
+features <- read_csv("~/R/Getting and Cleaning Data Course Project/UCI HAR Dataset/features.txt")  
+features<- as.character(features)
+ 
+#activities & subjects into factor
+MergTotalData$activity <- factor(MergTotalData$activity, levels = activityLabels[,1], labels = activityLabels[,2])
+MergTotalData$subject <- as.factor(MergTotalData$subject)
 
+MergTotalData.melted <- melt(MergTotalData, id = c("subject", "activity"))
+MergTotalData.mean <- dcast(MergTotalData.melted, subject + activity ~ variable, mean)
 
-
-
-
-
+write.table(MergTotalData.mean, "tidy.txt", row.names = FALSE, quote = FALSE)
