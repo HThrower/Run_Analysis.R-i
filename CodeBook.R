0a6a772ad35f50399dea2457fa0b79e47a7c88f7
HThrower <31675832+HThrower@users.noreply.github.com>
2017-12-01 18:58:24 -0500

    Add files via upload

---
+++ CodeBook.R
@@ -0,0 +1,33 @@
+CODE BOOK
+
+Identifiers
+*Subject -the name of the subject being tested
+Activity- the type of activity
+
+Activity Labels
+WALKING : subject was walking during the test
+WALKING_UPSTAIRS : subject was walking up a staircase during the test
+WALKING_DOWNSTAIRS : subject was walking down a staircase during the test
+SITTING : subject was sitting during the test
+STANDING : subject was standing during the test
+LAYING : subject was laying down during the test
+
+Variables
+x_train, y_train, x_test, y_test, subject_train and subject_test must be unziped and downloaded
+x_data, y_data and subject_data data must be merged before tiding up the data
+features is the names for columns
+MergTotalData is the name used for the merged data
+allData$activity
+
+Extras
+feature: for names that are better suited for R with some substitutions
+
+Mergeing data variable
+allData:training, testing
+colsWeWant:subject and activity
+
+removal of data
+allData
+
+Upload the data
+setwd()
